064ac397255b62559d0b5bc07da00936a3914750
Elaine <elaine.cadman@gmail.com>
2019-01-14 21:29:49 -0800

    schema and pulled exact API info

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,24 +2,32 @@ var db = require("../models");
 
 var unirest = require("unirest");
 
+var input = "tuna";
+
+var recipeNumber = 12023;
+
 //API CALL TO GET RECIPES
-// unirest
-//   .get(
-//     "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?number=6&offset=0&query=tuna"
-//   )
-//   .header(
-//     "X-RapidAPI-Key",
-//     "7QfEL0WGpbmshILA0RWQxq6VJZG8p1kt7PNjsnb3yQA9eSgW8O"
-//   )
-//   .end(function(result) {
-//     console.log("*****************RECIPE CALL*****************");
-//     console.log(result.status, result.headers, result.body);
-//   });
+unirest
+  .get(
+    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?number=6&offset=0&query="+input
+  )
+  .header(
+    "X-RapidAPI-Key",
+    "7QfEL0WGpbmshILA0RWQxq6VJZG8p1kt7PNjsnb3yQA9eSgW8O"
+  )
+  .end(function(result) {
+    console.log("*****************RECIPE CALL*****************");
+    console.log("Recipe Name: " + result.body.results[0].title);
+    console.log("id: " + result.body.results[0].id);
+    console.log("Image: " + result.body.results[0].image);
+  });
+
+
 
 //Finds ingredients based on Recipe index#
 unirest
   .get(
-    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/7539/information"
+    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + recipeNumber + "/information"
   )
   .header(
     "X-RapidAPI-Key",
@@ -31,10 +39,14 @@ unirest
     //   result.status,
     //   result.headers
     // );
+    // console.log(result.body);
     for (i = 0; i < result.body.extendedIngredients.length; i++) {
       console.log(result.body.extendedIngredients[i].name);
     }
   });
+
+
+
 
 module.exports = function(app) {
   app.get("/api/examples", function(req, res) {
